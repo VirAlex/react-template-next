674903710ddef000e48e84bffe5381335036f14c
Alexandre Viretti <alexou73@gmail.com>
2021-11-05 12:00:46 +0100

    feat personna select ok

--- components/Header/Personna.tsx
+++ components/Header/Personna.tsx
@@ -10,6 +10,59 @@ const SelectPersona = () => {
     { value: "RB_Ben", label: "Ben" },
     { value: "Neutral", label: "Neutral" },
   ];
+  const colourStyles = {
+    control: (styles) => ({
+      backgroundColor: "transparent",
+      display: "flex",
+      width: "100%",
+      position: "relative",
+    }),
+    placeholder: (styles) => ({
+      backgroundColor: "transparent",
+      fontSize: "1rem",
+      color: "black",
+      fontFamily: "Oswald",
+      textTransform: "uppercase",
+    }),
+    input: (styles) => ({ width: "100%" }),
+    container: (styles) => ({ width: "100%" }),
+    valueContainer: (styles) => ({
+      backgroundColor: "transparent",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+    }),
+    indicatorSeparator: (styles) => ({ display: "none" }),
+    dropdownIndicator: (styles) => ({ color: "black" }),
+    singleValue: (styles) => ({ top: "0", width: "50%" }),
+    menu: (styles) => ({
+      width: "8rem",
+      top: "2rem",
+      left: "0",
+      position: "absolute",
+      backgroundColor: "white",
+      borderRadius: "2px",
+      zIndex: "99",
+    }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return {
+        ...styles,
+        backgroundColor: isFocused ? "black" : "white",
+        color: isFocused ? "white" : "black",
+        fontSize: "0.8rem",
+        fontFamily: "Adobe Clean Light",
+        margin: "1rem",
+        transition: "all 0.2s",
+        cursor: "pointer",
+        width: "80%",
+        textAlign: "center",
+        textTransform: "uppercase",
+        fontWeight: "bold",
+        borderRadius: "2px",
+      };
+    },
+  };
   const dispatch = useDispatch();
 
   const selectValue = (e: any) => {
@@ -17,7 +70,12 @@ const SelectPersona = () => {
   };
 
   return (
-    <Select options={options} onChange={selectValue} placeholder={"Persona"} />
+    <Select
+      options={options}
+      onChange={selectValue}
+      placeholder={"Persona"}
+      styles={colourStyles}
+    />
   );
 };
 
